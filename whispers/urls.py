e6232b7e500e1536af7afd6b36ac8eaefc6b3050
Hillary Ogieleguea <hillariosky123@gmail.com>
2022-10-07 21:09:28 +0100

    update settings for environment vars

--- whispers/urls.py
+++ whispers/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
